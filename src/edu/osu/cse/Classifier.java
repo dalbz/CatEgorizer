20ce5918a9c9507e9eab9248e98b9eb1eb0c89ed
Dave <david.albert09@gmail.com>
2013-01-21 14:08:24 -0500

    Initial project setup

---
+++ src/edu/osu/cse/Classifier.java
@@ -0,0 +1,13 @@
+package edu.osu.cse;
+
+public class Classifier {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
