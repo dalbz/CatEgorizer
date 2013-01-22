6fdcd5fb796d2c00f252f181047d9c071bb57062
Dave <david.albert09@gmail.com>
2013-01-21 21:04:33 -0500

    Add code to extract and do edge detection on noses

--- src/edu/osu/cse/CatImage.java
+++ src/edu/osu/cse/CatImage.java
@@ -6,7 +6,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.HashMap;
 
 import javax.imageio.ImageIO;
 
@@ -14,9 +14,13 @@ public class CatImage {
 
     BufferedImage mImage;
 
-    ArrayList<BufferedImage> mSubImages;
+    HashMap<Feature, BufferedImage> mSubImages = new HashMap<CatImage.Feature, BufferedImage>();
 
     int[] mCoordinates = new int[18];
+
+    static enum Feature {
+        L_EAR, R_EAR, L_EYE, R_EYE, NOSE
+    }
 
     public CatImage(String filepath) {
 
@@ -62,6 +66,10 @@ public class CatImage {
 
             reader.close();
 
+            // get sub-images
+
+            extractSubImages();
+
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
@@ -69,5 +77,63 @@ public class CatImage {
         }
 
     }
+
+    private void extractSubImages() {
+
+        int imgWidth;
+        int leftX;
+        int topY;
+
+        // nose
+
+        imgWidth = (int) (euclideanDistance(mCoordinates[0], mCoordinates[1],
+                mCoordinates[2], mCoordinates[3]) / 2);
+
+        leftX = mCoordinates[4] - imgWidth / 2;
+        topY = mCoordinates[5] - imgWidth / 2;
+
+        BufferedImage nose = mImage
+                .getSubimage(leftX, topY, imgWidth, imgWidth);
+
+        mSubImages.put(Feature.NOSE, nose);
+
+
+        // eyes
+
+
+        // ears
+    }
+
+    private static double euclideanDistance(int x1, int y1, int x2, int y2) {
+        return Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+    }
+
+    public void save(String dataFolder, String fileName) {
+        try {
+
+            // save raw image
+
+            BufferedImage nose = mSubImages.get(Feature.NOSE);
+            File outputfile = new File(dataFolder + "/noses/raw/" + fileName);
+            ImageIO.write(nose, "png", outputfile);
+
+            // save processed image
+
+            CannyEdgeDetector detector = new CannyEdgeDetector();
+            detector.setLowThreshold(0.5f);
+            detector.setHighThreshold(1f);
+            // apply it to an image
+            detector.setSourceImage(nose);
+            detector.process();
+            BufferedImage edges = detector.getEdgesImage();
+            outputfile = new File(dataFolder + "/noses/processed/"
+                    + fileName);
+            ImageIO.write(edges, "png", outputfile);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 
 }
--- src/edu/osu/cse/Classifier.java
+++ src/edu/osu/cse/Classifier.java
@@ -10,6 +10,12 @@ public class Classifier {
         CatImage testCatImage = new CatImage(
                 "data/images/cats/00000001_000.jpg");
 
+        testCatImage.save("data/images", "00000001_000.png");
+
+        testCatImage = new CatImage("data/images/cats/00000001_005.jpg");
+
+        testCatImage.save("data/images", "00000001_005.png");
+
         System.out.println("Completed");
 
     }
